4dbd37c5964091ea6516cf5d24f46833a6f6c456
Michael-Bryant Choa <mbchoa@gmail.com>
2022-03-10 12:57:55 -0800

    fix(app): add missing title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import '../styles/globals.css';
-import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
+import Head from 'next/head';
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>flexin | A push-up a day</title>
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
