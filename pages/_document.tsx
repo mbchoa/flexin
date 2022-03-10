7c493185f13794f16584a151abaa1a55ebe2757e
Michael-Bryant Choa <mbchoa@gmail.com>
2022-03-10 12:45:11 -0800

    fix(document): add missing lang attribute to html

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
         <meta name="description" content="Daily push-up challenge tracker" />
         <meta name="theme-color" content="#FFFFFF" />
